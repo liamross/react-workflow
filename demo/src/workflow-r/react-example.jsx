3e2e85bda609b406286d7c72687d339bfcc137c4
Liam Ross <liamross@telus.net>
2018-01-17 01:41:02 -0800

    Basics of move and selection state

---
+++ demo/src/workflow-r/react-example.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './s.scss';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+function s({}) {
+  return (
+    <div className="s">
+      hi
+    </div>
+  );
+}
+
+s.propTypes = propTypes;
+s.defaultProps = defaultProps;
+export default s;
