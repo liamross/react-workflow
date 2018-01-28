d056afd976ecdf312f92c0c0963c8a3123ccce2f
Liam Ross <liamross@telus.net>
2018-01-27 21:10:59 -0800

    Deleted unnecessary demo folder, beginnings of manhattan routing

--- react-workflow-demo/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { WorkflowWorkspace } from './Workflow/WorkflowWorkspace';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<WorkflowWorkspace />, document.getElementById('root'));
-registerServiceWorker();
